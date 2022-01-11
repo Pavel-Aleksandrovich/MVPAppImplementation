5a22dbee04c89df04c277d43aaa3e9318fc6e73c
Pavel Mishanin <pavelmishanin@mail.ru>
2022-01-11 14:09:09 +0700

    Create extension for UIViewController: hide keyBoardWhenTappedAround

---
+++ MVPAppImplementation/Extensions/UIViewController Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController Extension.swift
+//  MVPAppImplementation
+//
+//  Created by pavel mishanin on 11.01.2022.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
